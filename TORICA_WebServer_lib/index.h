ca31c25a12c794d558ae8d80aedf9d3e0120f12a
Kennosuke Nagata <114tukaiyasui@gmail.com>
2025-04-18 12:15:50 +0900

    add comment

--- TORICA_WebServer_lib/index.h
+++ TORICA_WebServer_lib/index.h
@@ -56,7 +56,7 @@ static const char html[] = R"(
         .then(() => {
           setTimeout(() => {
             updateData();
-          }, 500);
+          }, 500); // Polling Interval
         })
         .catch(() => {
           console.error('Rejected getData');
@@ -76,7 +76,7 @@ static const char html[] = R"(
       }, 100);
     }
 
-    setTimeout(updateData,100);
+    setTimeout(updateData,100); // Function Start Timing
   </script>
 </html>
 
