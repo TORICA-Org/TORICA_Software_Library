8b01f514bdbe9367509bb46bc53de47c4a92cda6
00kenno <114tukaiyasui@gmail.com>
2025-04-27 01:35:21 +0900

    EventSource is no longer used.

---
+++ TORICA_WebServer_lib/src/TORICA_WebServer.h
@@ -0,0 +1,36 @@
+//Webサーバーに関するクラスの定義
+
+#ifndef TORICA_WEBSERVER_H
+#define TORICA_WEBSERVER_H
+
+#define WIFI_SSID "TORICA WebServer"
+#define WIFI_PASSWORD "toricadensou"
+
+#include <Arduino.h>
+
+#ifdef ARDUINO_ARCH_RP2040 //RP2040及びRP2350のチェックに対応
+#include <FreeRTOS.h>
+#include <task.h>
+#endif
+
+#include <WiFi.h>
+#include <DNSServer.h>
+#include <WebServer.h>
+
+#define LENGTH 32
+
+class TORICA_WebServer {
+  public:
+    TORICA_WebServer(char* p);
+    void begin();
+
+  private:
+    static int index;
+    static char data[LENGTH];
+    static char* _p;
+    static void handleRoot();
+    static void getData();
+    static void loop(void* param);
+};
+
+#endif // TORICA_WEBSERVER_H
