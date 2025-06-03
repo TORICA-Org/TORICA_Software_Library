9a5840976129ff2a43f9ab0a29165964b89ede04
Kennosuke Nagata <114tukaiyasui@gmail.com>
2025-06-03 21:03:30 +0900

    index

---
+++ TORICA_WebServer_lib/TORICA_WebServer_TEST/TORICA_WebServer_TEST.ino
@@ -0,0 +1,29 @@
+//TORICA_WebServer
+
+//WiFiを利用したスマートフォンやPCでデバッグ用モニターです．
+
+//「ログインが必要です」などと表示される画面（キャプティブポータル）で
+//任意の文字列（改行コードを含む）をモニターできます．
+
+//WiFiのSSIDは TORICA WebServer
+//WiFiのPASSWORDは toricadensou です．
+
+//2025.04.07時点で以下のマイコンでコンパイルが可能です．
+//ESP32C3
+//Raspberry Pi Pico W
+//Raspberry Pi Pico 2W
+
+#include "TORICA_WebServer.h"
+
+static char data[256]; // 静的メモリ領域に配列を作成
+TORICA_WebServer webserver(data);
+
+void setup() {
+  webserver.begin();
+}
+
+void loop() {
+  uint32_t now = millis();
+  sprintf(data, "改行コードが使用可能です．\n\n以下は実行を開始してからの時間をミリ秒で表示しています．\n%d", now);
+  delay(100);
+}
